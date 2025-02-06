839c27375f0a506930471b6621a64ba8825866c0
kazimorwan498 <kazimdmorwan498@gmail.com>
2025-02-06 22:46:12 +0600

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<h1 className="text-8xl font-bold bg-red-600 text-white">
+				Hello World
+			</h1>
+		</>
+	);
+}
+
+export default App;
